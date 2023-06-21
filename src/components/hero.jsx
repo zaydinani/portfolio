012bf894521dc3892305f7153683eedf9c0908be
zaydinani <zaydinani@protonmail.com>
2023-06-22 00:59:32 +0100

    hero
    added hero section and some tweaks to nav bar

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,8 +3,19 @@ import "../styles/hero.scss"
 
 function Hero() {
     return (
-        <div className="section hero">
-            <h1>hero</h1>
+        <div className="section">
+            <div className="hero">
+                <div>
+                    <h1>hi, my name is zayd inani, i'm a developer from morocco</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis deserunt fuga, consectetur dignissimos vitae laborum repellat ea alias quos</p>
+                </div>
+                <div>
+                    <ul>
+                        <li className=" btn main-btn"><a href="">Contact</a></li>
+                        <li className="btn secondary-btn"><a href="">Projects</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
