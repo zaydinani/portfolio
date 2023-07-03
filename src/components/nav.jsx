f0959811a27ac1717ca85c8a85f2cf130e3e82bd
zaydinani <zaydinani@protonmail.com>
2023-07-03 16:59:01 +0100

    switching to react

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,6 +3,17 @@ import "../styles/nav.scss"
 
 
 function Nav() {
+    const setDarkMode = () => {
+        document.querySelector("body").setAttribute("data-theme", "dark");
+    };
+    const setLightMode = () => {
+        document.querySelector("body").setAttribute("data-theme", "light");
+    };
+
+    const toggleTheme = (e) => {
+        if (e.target.checked) setLightMode()
+        else setDarkMode();
+    }
     return (
         <nav>
             <ul>
@@ -11,7 +22,7 @@ function Nav() {
                 <li><a href="">projects</a></li>
                 <li><a href="">contact</a></li>
                 <div>
-                    <input type="checkbox" className="checkbox" id="checkbox"/>
+                    <input type="checkbox" className="checkbox" id="checkbox" onChange={toggleTheme}/>
                     <label for="checkbox" className="label">
                         <img className="fa-moon" src="/moon purple.png"alt=""/>
                         <img className="fa-sun" src="/sun-solid.png" alt=""/>
