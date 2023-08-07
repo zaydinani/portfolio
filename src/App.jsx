0911005fda6d22a6e593a72758daea835d6b45f1
zaydinani <zaydinani@protonmail.com>
2023-08-07 18:38:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './styles/app.scss'
+import './styles/App.scss'
 import Nav from './components/nav'
 import Main from './pages/main'
 import Footer from './components/footer'
