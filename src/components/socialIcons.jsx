457957628f6f1e1f31c440cc4486324189f99e8e
zaydinani <zaydinani@protonmail.com>
2023-09-15 18:43:11 +0100

    added google analytics and twitter ioon

--- src/components/socialIcons.jsx
+++ src/components/socialIcons.jsx
@@ -46,6 +46,18 @@ function socials() {
                     </g>
                 </svg>
             </a>
+            {/*twitter / X */}
+            <a href={data['zayd-data'].about.socials[4].linkUrl} target="_blank">
+                <svg  className="socialIcons" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 256 256" width="144px" height="144px">
+                    <g fill="none" fillRule="none" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none" style={{ mixBlendMode: 'normal' }}>
+                        <g transform="scale(5.33333,5.33333)">
+                            <path className="Vector" d="M41,6l-31.071,36h-3.714l31.072,-36z" fill="#000000" fillRule="nonzero"></path>
+                            <path className="Vector_2" d="M31.143,41l-23.323,-34h8.957l23.323,34z" fill="#ffffff" fillRule="evenodd"></path>
+                            <path className="Vector_3" d="M15.724,9l20.578,30h-4.106l-20.578,-30h4.106M17.304,6h-11.382l24.694,36h11.382l-24.694,-36z" fill="#000000" fillRule="nonzero"></path>
+                        </g>
+                    </g>
+                </svg>
+            </a>
         </div>
     );
 }
